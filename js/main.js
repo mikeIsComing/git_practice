287ac79704b79ee24771e9b542bf8cce9fa8bba7
MikeDev <callmemajsa@seznam.cz>
2023-02-20 22:37:30 +0100

    fix switch mode menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-const openSwitchMenuBtn = document.getElementById("open-SM-menu");
-
 function openSwitchMenu() {
   document.getElementById("show-SM-menu").style.display = "block";
 }
 
+// function for change to dark mode
 
+function switchToDark() {
+  document.body.style.backgroundColor = "#4d4f53";
+  document.body.style.color = "#fff";
+  document.getElementById("show-SM-menu").style.display = "none";
+}
+
+// function for change to light mode
+
+function switchToLight() {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+  document.getElementById("show-SM-menu").style.display = "none";
+}
